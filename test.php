ce17fba0874ad449d582a5fba6d55f189bae78c4
lizlit <lis.litvinova@gmail.com>
2019-10-14 21:24:26 +0700

    //

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
 $db_connect=pg_connect("host=localhost port=5432 dbname=postgres user=postgres");
-var_dump($db_connect);
-
+//var_dump($db_connect);
+if ($db_connect) echo 'Successful connection';
 echo "123";
